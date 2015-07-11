92ce3c8d18db8fdfd35479794abe915c22427894
re0marb1e <LHxSS@sns5-210.york.ac.uk>
2015-07-11 19:10:25 +0100

    origin

---
+++ Project/Wireless/src/delay.c
@@ -0,0 +1,35 @@
+#include "stm32f4xx.h"
+
+static uint32_t TimingDelay;
+extern uint32_t SystemCoreClock;
+
+void SysTick_Configuration(void)
+{
+    if(SysTick_Config(SystemCoreClock / 1000)) {
+        while(1);
+    }
+}
+
+void mdelay(u32 ms)
+{
+    TimingDelay = ms;
+
+    while(TimingDelay != 0);
+}
+
+void TimingDelay_Decrement(void)
+{
+    if (TimingDelay != 0x00) {
+        TimingDelay--;
+    }
+}
+
+/**
+  * @brief  This function handles SysTick Handler.
+  * @param  None
+  * @retval None
+  */
+void SysTick_Handler(void)
+{
+    TimingDelay_Decrement();
+}
